1db8d36bf3191f183b61a7adf2627774fc94c276
Jinwoo Lee <blackbox0209@gmail.com>
2024-09-04 17:35:15 +0900

    Fix: Tag selection from external page
    Fix: Further Optimized Tag Selection logic

---
+++ src/components/Tag/TagAccordion.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState, useCallback } from "react";
+import TagSelector from "./TagSelector";
+import TagChildSelector from "./TagChildSelector";
+import { tagLoader, tagElementLoader } from "./TagLoader";
+import { isEmpty } from "../../provider/utilityProvider";
+
+function TagAccordion({
+  selectedTag,
+  setTag,
+  selectedElement,
+  setElement,
+  onTagChange,
+  onElementChange,
+}) {
+  const [tagData, setTagData] = useState([]);
+  const [tagElementData, setTagElementData] = useState([]);
+
+  // Load Tag Data on initial build
+  useEffect(() => {
+    tagLoader().then((data) => {
+      setTagData(data);
+    });
+  }, []);
+
+  const loadTagElements = useCallback(() => {
+    // Update Tag Child
+    if (selectedTag) {
+      tagElementLoader(selectedTag).then(setTagElementData);
+    } else {
+      setTagElementData([]);
+    }
+    // Update Search Key Object
+    onTagChange();
+  }, [selectedTag, onTagChange]);
+
+  // on Main Tag Change
+  useEffect(() => {
+    loadTagElements();
+  }, [selectedTag]);
+
+  // on Tag Child Change
+  useEffect(() => {
+    onElementChange();
+  }, [selectedElement]);
+
+  return (
+    <>
+      {isEmpty(tagData) && <></>}
+      {!isEmpty(tagData) && (
+        <>
+          <TagSelector
+            title="작업 분야"
+            tagList={tagData}
+            selectedTag={selectedTag}
+            setTag={setTag}
+            toggle={true}
+          />
+          {tagElementData &&
+            tagElementData.map((subTag, index) => (
+              <TagChildSelector
+                key={index}
+                title={subTag.subCategoryName}
+                tagList={subTag.workFieldChildTagResponseDtoList}
+                selectedElement={selectedElement}
+                setElement={setElement}
+              />
+            ))}
+        </>
+      )}
+    </>
+  );
+}
+
+export default React.memo(TagAccordion);
