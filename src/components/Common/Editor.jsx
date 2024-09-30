111dac9eaf955db757f955219f280d05bff99bbb
Jinwoo Lee <blackbox0209@gmail.com>
2024-10-01 03:24:36 +0900

    Feat: Youtube iFrame Support for Editor

--- src/components/Common/Editor.jsx
+++ src/components/Common/Editor.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  DialogTitle,
+  TextField,
+} from "@mui/material";
+
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Link from "@tiptap/extension-link";
 import Underline from "@tiptap/extension-underline";
 import TextAlign from "@tiptap/extension-text-align";
+import Youtube from "@tiptap/extension-youtube";
 import classes from "./Editor.module.css";
 
 import {
@@ -18,96 +30,172 @@ import {
   RiAlignLeft,
   RiAlignCenter,
   RiAlignRight,
+  RiYoutubeLine,
 } from "@remixicon/react";
 import { Divider } from "@mui/material";
 
 export const EditorMenuBar = ({ editor }) => {
+  const [isYoutubeOpen, setIsYoutubeOpen] = useState(false);
+  const [isURLokay, setIsURLokay] = useState(true);
+
   if (!editor) {
     return null;
   }
 
+  const handleYoutubeOpen = () => {
+    setIsYoutubeOpen(true);
+  };
+  const handleYoutubeClose = () => {
+    setIsYoutubeOpen(false);
+  };
+  const addYoutubeVideo = (e) => {
+    e.preventDefault();
+    const youtubeVideoUrl = e.target.elements.youtubeVideoUrl.value;
+
+    if (youtubeVideoUrl.includes("youtube.com/watch?v=")) {
+      editor.commands.setYoutubeVideo({
+        src: youtubeVideoUrl,
+      });
+      handleYoutubeClose();
+      setIsURLokay(true);
+    } else {
+      setIsURLokay(false);
+    }
+  };
+
   return (
-    <div className={classes.editorMenuBar}>
-      <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        disabled={!editor.can().chain().focus().toggleBold().run()}
-        className={editor.isActive("bold") ? "is-active" : ""}
+    <>
+      <div className={classes.editorMenuBar}>
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          disabled={!editor.can().chain().focus().toggleBold().run()}
+          className={editor.isActive("bold") ? "is-active" : ""}
+        >
+          <RiBold size={20} />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          disabled={!editor.can().chain().focus().toggleStrike().run()}
+          className={editor.isActive("strike") ? "is-active" : ""}
+        >
+          <RiStrikethrough2 size={20} />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleUnderline().run()}
+          disabled={!editor.can().chain().focus().toggleUnderline().run()}
+          className={editor.isActive("underline") ? "is-active" : ""}
+        >
+          <RiUnderline size={20} />
+        </button>
+        <Divider
+          orientation="vertical"
+          variant="middle"
+          flexItem
+          sx={{ borderColor: "#aeaba7" }}
+        />
+        <button
+          onClick={() => editor.chain().focus().setTextAlign("left").run()}
+        >
+          <RiAlignLeft size={20} />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setTextAlign("center").run()}
+        >
+          <RiAlignCenter size={20} />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setTextAlign("right").run()}
+        >
+          <RiAlignRight size={20} />
+        </button>
+        <Divider
+          orientation="vertical"
+          variant="middle"
+          flexItem
+          sx={{ borderColor: "#aeaba7" }}
+        />
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={editor.isActive("bulletList") ? "is-active" : ""}
+        >
+          <RiListUnordered size={20} />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={editor.isActive("orderedList") ? "is-active" : ""}
+        >
+          <RiListOrdered2 size={20} />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().setHorizontalRule().run()}
+        >
+          <RiSubtractLine size={20} />
+        </button>
+        <Divider
+          orientation="vertical"
+          variant="middle"
+          flexItem
+          sx={{ borderColor: "#aeaba7" }}
+        />
+        <button onClick={handleYoutubeOpen}>
+          <RiYoutubeLine size={20} />
+        </button>
+        <Divider
+          orientation="vertical"
+          variant="middle"
+          flexItem
+          sx={{ borderColor: "#aeaba7" }}
+        />
+        <button
+          onClick={() => editor.chain().focus().undo().run()}
+          disabled={!editor.can().chain().focus().undo().run()}
+        >
+          <RiArrowGoBackLine size={20} />
+        </button>
+        <button
+          onClick={() => editor.chain().focus().redo().run()}
+          disabled={!editor.can().chain().focus().redo().run()}
+        >
+          <RiArrowGoForwardLine size={20} />
+        </button>
+      </div>
+      <Dialog
+        open={isYoutubeOpen}
+        onClose={handleYoutubeClose}
+        PaperProps={{
+          component: "form",
+          onSubmit: (e) => {
+            addYoutubeVideo(e);
+            setIsURLokay(true);
+          },
+        }}
       >
-        <RiBold size={20} />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-        disabled={!editor.can().chain().focus().toggleStrike().run()}
-        className={editor.isActive("strike") ? "is-active" : ""}
-      >
-        <RiStrikethrough2 size={20} />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleUnderline().run()}
-        disabled={!editor.can().chain().focus().toggleUnderline().run()}
-        className={editor.isActive("underline") ? "is-active" : ""}
-      >
-        <RiUnderline size={20} />
-      </button>
-      <Divider
-        orientation="vertical"
-        variant="middle"
-        flexItem
-        sx={{ borderColor: "#aeaba7" }}
-      />
-      <button onClick={() => editor.chain().focus().setTextAlign("left").run()}>
-        <RiAlignLeft size={20} />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setTextAlign("center").run()}
-      >
-        <RiAlignCenter size={20} />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setTextAlign("right").run()}
-      >
-        <RiAlignRight size={20} />
-      </button>
-      <Divider
-        orientation="vertical"
-        variant="middle"
-        flexItem
-        sx={{ borderColor: "#aeaba7" }}
-      />
-      <button
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive("bulletList") ? "is-active" : ""}
-      >
-        <RiListUnordered size={20} />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive("orderedList") ? "is-active" : ""}
-      >
-        <RiListOrdered2 size={20} />
-      </button>
-      <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
-        <RiSubtractLine size={20} />
-      </button>
-      <Divider
-        orientation="vertical"
-        variant="middle"
-        flexItem
-        sx={{ borderColor: "#aeaba7" }}
-      />
-      <button
-        onClick={() => editor.chain().focus().undo().run()}
-        disabled={!editor.can().chain().focus().undo().run()}
-      >
-        <RiArrowGoBackLine size={20} />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().redo().run()}
-        disabled={!editor.can().chain().focus().redo().run()}
-      >
-        <RiArrowGoForwardLine size={20} />
-      </button>
-    </div>
+        <DialogTitle>유튜브 영상 첨부</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {isURLokay
+              ? "첨부할 유튜브 영상의 주소를 입력해주세요."
+              : "올바른 유튜브 영상 URL을 입력해주세요."}
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="youtubeVideoUrl"
+            label="유튜브 영상 URL"
+            type="url"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions sx={{ padding: "0 1rem 1rem 1rem" }}>
+          <Button onClick={handleYoutubeClose}>취소</Button>
+          <Button type="submit" variant="contained">
+            추가
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
 
@@ -118,15 +206,15 @@ export const ReadOnlyEditor = ({ content }) => {
   } catch (e) {
     contentData = [
       {
-        type: 'paragraph',
+        type: "paragraph",
         content: [
           {
-            type: 'text',
+            type: "text",
             text: `${content}`,
           },
         ],
       },
-    ]
+    ];
   }
 
   const editor = useEditor({
@@ -156,12 +244,15 @@ export const editorExtensions = [
   StarterKit,
   Underline,
   TextAlign.configure({
-    types: ["paragraph"],
+    types: ['paragraph', 'div[data-youtube-video]'],
     alignments: ["left", "center", "right"],
   }),
   Link.configure({
     openOnClick: true,
     autolink: true,
     rel: "noopener noreferrer",
+  }),
+  Youtube.configure({
+    inline: true,
   }),
 ];
