69410ae766a3eb0f232a39cee0173c53821f6e47
Jinwoo Lee <blackbox0209@gmail.com>
2024-10-12 22:02:16 +0900

    Fix: Notification Provider behavior
    Fixed an issue where Notification Provider was running on logout state

--- src/provider/notificationProvider.jsx
+++ src/provider/notificationProvider.jsx
@@ -1,13 +1,18 @@
 import React, { createContext, useState, useEffect, useContext } from "react";
+import { useAuth } from "./authProvider";
 import apiInstance from "./networkProvider";
 
 const NotificationContext = createContext();
 
 export default function NotificationProvider({ children }) {
+  const { isLoggedIn } = useAuth();
   const [hasUnreadChat, setHasUnreadChat] = useState(false);
   const [hasUnreadNotifications, setHasUnreadNotifications] = useState(false);
 
   const checkForNotifications = async () => {
+    if (!isLoggedIn) return;
+
+    console.log("checkForNotifications");
     try {
       const response = await apiInstance.get("/api/v1/notify/check");
       if (response.status === 200) {
@@ -20,16 +25,17 @@ export default function NotificationProvider({ children }) {
       //Toast.error("데이터를 불러오는 중 오류가 발생했습니다.");
       console.error("Error fetching notifications:", error);
     }
-    return {};
   };
 
   useEffect(() => {
-    checkForNotifications();
-    // Set up an interval to check for notifications periodically
-    const intervalId = setInterval(checkForNotifications, 60000); // Check every minute
+    if (isLoggedIn) {
+      checkForNotifications();
+      // Set up an interval to check for notifications periodically
+      const intervalId = setInterval(checkForNotifications, 60000); // Check every minute
 
-    return () => clearInterval(intervalId);
-  }, []);
+      return () => clearInterval(intervalId);
+    }
+  }, [isLoggedIn]);
 
   return (
     <NotificationContext.Provider
